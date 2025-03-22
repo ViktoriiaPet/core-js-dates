fe0da813ae68943fd75a813d53ce2ec34d203480
Viktoriia Pet <viktoriiap0205199@gmail.com>
2025-03-22 15:20:26 +0100

    feat: add resolve of 2 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -94,8 +94,8 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -109,8 +109,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const startTime = new Date(dateStart);
+  const finTime = new Date(dateEnd);
+  const rest = (finTime.getTime() - startTime.getTime()) / 86400000 + 1;
+  return rest;
 }
 
 /**
