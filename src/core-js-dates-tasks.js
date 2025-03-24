3ed5311ca8e0eabdf3f061e9632ff83c09239a2a
Viktoriia Pet <viktoriiap0205199@gmail.com>
2025-03-24 17:32:11 +0100

    feat: add 2 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -133,8 +133,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const currentDate = new Date(date).setHours(0, 0, 0, 0);
+  const start = new Date(period.start).setHours(0, 0, 0, 0);
+  const end = new Date(period.end).setHours(0, 0, 0, 0);
+
+  return currentDate >= start && currentDate <= end;
 }
 
 /**
@@ -148,8 +152,18 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const typingDate = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true,
+    timeZone: 'UTC',
+  };
+  return new Intl.DateTimeFormat('en-US', typingDate).format(new Date(date));
 }
 
 /**
